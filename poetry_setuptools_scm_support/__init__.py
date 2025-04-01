96136d7af117c70999dda824aee2eece765bd7b2
luca <laxertu@gmail.com>
2025-04-01 12:58:42 +0200

    0.3.0

--- poetry_setuptools_scm_support/__init__.py
+++ poetry_setuptools_scm_support/__init__.py
@@ -3,8 +3,7 @@ from cleo.io.inputs.argument import Argument
 from poetry.plugins.application_plugin import ApplicationPlugin
 from poetry.factory import Factory
 
-from setuptools_scm import Configuration, ScmVersion, get_version
-from setuptools_scm._get_version_impl import parse_scm_version
+from setuptools_scm import Configuration, get_version
 from setuptools_scm.version import guess_next_date_ver
 
 import warnings
@@ -25,34 +24,21 @@ class CalculateVersion(Command):
 
     args_description = """
         scm: formats according to setuptools_scm default behavior. e.g. 0.1.dev1+g1e0ede4
-        date: formats using last commit distance and dirty tag, e.g. 2025.3.31.1.dev1+g1e0ede4
-        dist: formats using last commit distance only, e.g. 2025.3.31.1.dev1
+        date: formats using last commit distance and dirty tag, e.g. 2025.3.31.1+g1e0ede4
     """
     arguments = [
         Argument(name="format", description=args_description, default="scm", required=False),
     ]
 
 
-    def __do_default(self, c: Configuration) -> str:
+    def __do_scm(self, c: Configuration) -> str:
         return get_version(root=c.root, relative_to=c.relative_to)
 
-    def __do_inc(self, c: Configuration) -> str:
-        scm_version: ScmVersion = parse_scm_version(c)
+    def __do_date_and_dirty(self, c: Configuration) -> str:
 
-        return scm_version.format_next_version(
-            guess_next_date_ver,
-            date_fmt="%Y.%m.%d",
-            fmt="{guessed}.dev{distance}+{node}",
-        )
+        # fmt="{guessed}.dev{distance}+{node}"
+        return get_version(root=c.root, relative_to=c.relative_to, version_scheme=guess_next_date_ver)
 
-    def __do_distance(self, c: Configuration) -> str:
-        scm_version: ScmVersion = parse_scm_version(c)
-
-        return scm_version.format_next_version(
-            guess_next_date_ver,
-            date_fmt="%Y.%m.%d",
-            fmt="{guessed}.dev{distance}",
-        )
 
 
     def handle(self) -> int:
@@ -63,11 +49,9 @@ class CalculateVersion(Command):
 
             format_to_use = self.argument("format")
             if format_to_use == "scm":
-                v = self.__do_default(c)
-            elif format_to_use == "dist":
-                v = self.__do_distance(c)
+                v = self.__do_scm(c)
             elif format_to_use == "date":
-                v = self.__do_inc(c)
+                v = self.__do_date_and_dirty(c)
             else:
                 self.line_error(f"Unknown format: {format_to_use}")
                 return 0
